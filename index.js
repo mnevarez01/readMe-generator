8c06ab90c391e08f24d496e83a10699e8c453185
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-15 14:54:17 -0600

    added questions to file

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 var inquirer = require("inquirer");
-var fs = require("fs")
+var fs = require("fs");
+var util = require("util");
 
-
+const writeReadAsync = util.promisify(fs.writeFile);
+//inside the const questions?
 
 const questions = [
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your github username?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "What is your email?",
+            name: "email"
+        },
 
+        {
+            type: "input",
+            message: "What is the tile of your project?",
+            name: "projectName"
+
+        },
+        {
+            type: "input",
+            message: "What is a good description of your project?",
+            name: "description"
+
+        },
+        {
+            type: "input",
+            message: "What command would you need to clone?",
+            name: "clone"
+
+        },
+        {
+            type: "input",
+            message: "How would you run a test?",
+            name: "testing"
+
+        },
+        {
+            type: "input",
+            message: "What do we need to know to contribute?",
+            name: "contribute"
+
+        },
+
+    ])
 ];
 
 function writeToFile(fileName, data) {
